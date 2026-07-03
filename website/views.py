3f5c64fabe2ba8709b0be39d999a21fd13e37a03
Hadi <hadishayeg7275@gmail.com>
2026-07-03 13:59:54 +0330

    feat: add templates for website pages

--- website/views.py
+++ website/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 def index_view(request):
-    return HttpResponse("<h1>Welcome to the Home Page!</h1>")
+    return render(request, 'website/index.html')
 
 def about_view(request):
-    return HttpResponse("<h1>This is the About Page!</h1>")
+    return render(request, 'website/about.html')
 
 def contact_view(request):
-    return HttpResponse("<h1>This is the Contact Page!</h1>")
+    return render(request, 'website/contact.html')
